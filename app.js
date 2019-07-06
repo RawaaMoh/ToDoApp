bbd9853fc738b9da1eda76c7f5071c6fe3512396
Rawaa Rajab <rawaa.rajab@gmail.com>
2019-07-06 12:27:15 +0300

    still working on it

---
+++ app.js
@@ -0,0 +1,36 @@
+let smthn = prompt("what do u want from the list?");
+
+function list() {
+    for (let i = 0; i < arr.length; i++) {
+        if (smthn == "new") {
+            newItem();
+        }
+        else if (smthn == "list") {
+            listItem();
+        }
+        else if (smthn == "delete") {
+            deleteItem();
+        }
+        else if (smthn == "quit") {
+            quitApp();
+        }
+    }
+}
+
+function newItem(item) {
+    for (let i = 0; i < arr.length; i++) {
+arr.push();
+    }
+}
+
+function listItem(item) {
+    for (let i = 0; i < arr.length; i++) { }
+}
+
+function deleteItem(item) {
+    for (let i = 0; i < arr.length; i++) { }
+}
+
+function quitApp(item) {
+    for (let i = 0; i < arr.length; i++) { }
+}
