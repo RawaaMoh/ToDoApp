7c79816006e65efed912ae64a9ce3d94ec548721
Rawaa Rajab <rawaa.rajab@gmail.com>
2019-07-08 12:29:17 +0300

    still testing

--- app.js
+++ app.js
@@ -1,51 +1,64 @@
-let smthn = prompt("what do u want from the list?");
+let option = prompt("what do u want from the list?");
 
 let todoItems = [];
 
-function list() {
-    for (let i = 0; i < arr.length; i++) {
-        if (smthn == "new") {
-            newItem();
-        }
-        else if (smthn == "list") {
-            listItem();
-        }
-        else if (smthn == "delete") {
-            deleteItem();
-        }
-        else if (smthn == "quit") {
-            quitApp();
-        }
-    }
+const text = input.value.trim();
+if (text !== '') {
+  addTodo(text);
+  input.value = '';
+  input.focus();
 }
 
-function newItem(text) {
+// function list() {
+
+// for (let i = 0; i < arr.length; i++) {
+
+if (option == "new") {
+
+  function addTodo(text) {
     const todo = {
-        text,
-        checked: false,
-        id: Date.now(),
+      text,
+      checked: false,
     };
 
     todoItems.push(todo);
-    console.log(todoItems);
-}
 
+  }
 
-function listItem(item) {
-    let arr = [];
-    for (let i = 0; i < arr.length; i++) {
+} //new
 
-    }
-}
+else if (option == "list") {
 
-function deleteItem(item) {
-    for (let i = 0; i < arr.length; i++) {
-        
-    }
-}
+  function listItem(text) {
 
-function quitApp(item) {
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < todoItems.length; i++) {
 
     }
-}
+  }
+
+}//list
+
+else if (option == "delete") {
+
+  function deleteItem(text) {
+    for (let i = 0; i < todoItems.length; i++) {
+
+    }
+  }
+
+} //delete
+
+else if (option == "quit") {
+
+  function quitApp(text) {
+    for (let i = 0; i < todoItems.length; i++) {
+
+    }
+  }
+
+} //quit
+
+// } //for(i) loop
+
+// } //list function
+
